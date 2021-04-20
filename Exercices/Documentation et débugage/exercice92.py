6f2e2013fb0ea2fc9f5690ffee8abe0feb743fd4
Zidiboo <jmear92@gmail.com>
2021-04-20 11:57:55 +0200

    pages faites et modification du dossier "Exercices"

--- Exercices/Documentation et débugage/exercice92.py
+++ Exercices/Documentation et débugage/exercice92.py
@@ -1,14 +1,14 @@
-def est_croissante(t):
-    i = len(t) - 1
-    while i >= 0:
-        if t[i] <= t[i+1]:
-            print(True)
-            return True
+def est_croissant (t):
+    if len(t)==0:
+        return None
+    i=len(t)-1
+    while i>0:
+#        print("nouveau tour avec i =" , i)
+        if t[i-1]<=t[i]:
+            i-=1
         else:
-            print(False)
             return False
-    i -= 1
-
-#est_croissante([])
-assert est_croissante([1,2,3,4]), "like ou meurs demain"
-assert est_croissante([4,3,2,1])==False, "conséquences"
+    return True
+assert est_croissant ([1,2,3,4])==True
+assert est_croissant ([1,3,2,4])==False
+assert est_croissant ([])==None
