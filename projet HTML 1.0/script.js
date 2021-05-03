5837391eca55300c073926aa48d9834fb8c84012
Zidiboo <jmear92@gmail.com>
2021-05-03 11:26:00 +0200

    bouton scroll

--- projet HTML 1.0/script.js
+++ projet HTML 1.0/script.js
@@ -4,11 +4,30 @@ var txt = document.querySelector('p');
 btn.addEventListener('click', updateBtn);
 
 function updateBtn() {
-  if (btn.value === 'Démarrer la machine') {
-    btn.value = 'Arrêter la machine';
-    txt.textContent = 'La machine est démarrée !';
+  if (btn.value === 'Télécharger') {
+    btn.value = 'Téléchargé';
   } else {
     btn.value = 'Démarrer la machine';
     txt.textContent = 'La machine est arrêtée.';
   }
 }
+
+
+
+
+
+
+
+ 
+jQuery(function(){
+    $(function () {
+        $(window).scroll(function () { //Fonction appelée quand on descend la page
+            if ($(this).scrollTop() > 200 ) {  // Quand on est à 200pixels du haut de page,
+                $('#scrollUp').css('right','10px'); // Replace à 10pixels de la droite l'image
+            } else { 
+                $('#scrollUp').removeAttr( 'style' ); // Enlève les attributs CSS affectés par javascript
+            }
+        });
+    });
+});
+ 
