b1cce512b796b4102d3dffd3ea2fa8d2be7db141
Syssy-1e <78019545+Syssy-1e@users.noreply.github.com>
2021-05-17 10:55:30 +0200

    Correction ajoutées

--- projet HTML fini+/Exercices/Boucles/exercice39.py
+++ projet HTML fini+/Exercices/Boucles/exercice39.py
@@ -2,15 +2,15 @@
 
 
 
-def conv_base10_to_base2(x):
+def conv_base2_to_base10(x):
     n = 1
     c = 0
     while x > 0:
         if x % 2 == 1 :
             c += n
             x -= 1
-        n = 10*n
-        x = x/2
+        n = 2*n
+        x = x/10
     return c
 
 
